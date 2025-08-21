7aa6bf85c2555950618d95fe65b8f3dd28e8c761
leyla1223 <leylamuradli2016@gmail.com>
2025-08-21 12:29:49 +0000

    try1

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lockboxes interview task"""
+
+def canUnlockAll(boxes):
+    """Determines if all boxes can be opened"""
+    if not isinstance (boxes, list) or len(boxes) == 0:
+        return False
+
+    n = len(boxes)
+    opened = set([0])
+    keys = boxes[0].copy()
+
+    while keys:
+        key = keys.pop()
+        if key < n and key not in opened:
+            opened.add(key)
+            keys.extend(boxes[key])
+
+    return len(opened) == n        
